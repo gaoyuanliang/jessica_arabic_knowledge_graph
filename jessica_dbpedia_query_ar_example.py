4b97205b12d2cfac1af1a4f53dea1530c6bacabe
Jessica Gaoyuan Liang <gaoyuanliang@outlook.com>
2020-12-09 20:41:58 +0400

    Add files via upload

--- jessica_dbpedia_query_ar_example.py
+++ jessica_dbpedia_query_ar_example.py
@@ -1,18 +1,5 @@
+########jessica_dbpedia_query_ar_example.py########
 from jessica_dbpedia_query import *
-
-input_triplets = [
-{'object': 'http://ar.dbpedia.org/resource/دبي', 'relation': 'http://dbpedia.org/ontology/deathPlace', 'subject': 'http://ar.dbpedia.org/resource/عبد_الرحمن_بن_حافظ'},
-{'object': 'http://ar.dbpedia.org/resource/دبي', 'relation': 'http://dbpedia.org/ontology/deathPlace', 'subject': 'http://ar.dbpedia.org/resource/عبد_الرحمن_بن_حافظ'},
-{'object': 'http://ar.dbpedia.org/resource/دبي', 'relation': 'http://dbpedia.org/ontology/deathPlace', 'subject': 'http://ar.dbpedia.org/resource/عبد_الرحمن_بن_حافظ'},
-{'object': 'http://ar.dbpedia.org/resource/دبي', 'relation': 'http://dbpedia.org/ontology/birthPlace', 'subject': 'http://ar.dbpedia.org/resource/عبد_الرحمن_بن_حافظ'},
-]
-
-output_triplets, entity_type_lookup, entity_name_lookup, relation_name_lookup = attach_triplet_type_and_name(input_triplets)
-
-for t in output_triplets:
-	print(t)
-
-###
 
 from yan_neo4j import start_neo4j
 from yan_neo4j import create_neo4j_session
@@ -21,10 +8,6 @@ from yan_neo4j import ingest_knowledge_triplets_to_neo4j
 start_neo4j(http_port = "5987", bolt_port = "4522")
 neo4j_session = create_neo4j_session(bolt_port = "4522")
 
-ingest_knowledge_triplets_to_neo4j(output_triplets, neo4j_session)
-
-
-
 t = find_related_entities(
 	entity_id = "http://ar.dbpedia.org/resource/دبي",
 	related_subject_num = 4,
@@ -32,5 +15,5 @@ t = find_related_entities(
 
 output_triplets, entity_type_lookup, entity_name_lookup, relation_name_lookup = attach_triplet_type_and_name(t)
 
-
 ingest_knowledge_triplets_to_neo4j(output_triplets, neo4j_session)
+########jessica_dbpedia_query_ar_example.py########
