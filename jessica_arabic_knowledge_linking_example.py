9908d03b502ef544f0ba3ac3a577b1ad72e93747
Jessica Gaoyuan Liang <gaoyuanliang@outlook.com>
2020-12-11 22:13:57 +0400

    Add files via upload

---
+++ jessica_arabic_knowledge_linking_example.py
@@ -0,0 +1,8 @@
+######jessica_arabic_knowledge_linking_example.py#######
+
+from jessica_arabic_knowledge_linking import arabic_text_knowledge_linking
+
+text = u"أنا من جدة ولكني الآن أدرس في الرياض. أحيانًا أزور دبي من أجل المتعة."
+arabic_text_knowledge_linking(text)
+
+######jessica_arabic_knowledge_linking_example.py#######
