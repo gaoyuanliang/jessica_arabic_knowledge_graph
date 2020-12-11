752b6cc1110b6f36476437c29ad1fb22941cec43
Jessica Gaoyuan Liang <gaoyuanliang@outlook.com>
2020-12-11 20:59:43 +0400

    Add files via upload

---
+++ jessica_dbpedia_es_search_example.py
@@ -0,0 +1,9 @@
+####jessica_dbpedia_es_search_example.py####
+
+from jessica_dbpedia_es_search import search_es_entity
+
+search_es_entity(u"أبو ظبي")
+
+[{'entity_id': 'http://ar.dbpedia.org/resource/أبو_ظبي', 'entity_name': 'أبو ظبي'}]
+
+####jessica_dbpedia_es_search_example.py####
